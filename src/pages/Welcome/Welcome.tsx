2d533011bdcaec96a4d7ce0e3c0eccfa115dc090
fN9ne <seniorderzhavin@yandex.ru>
2024-05-05 22:53:23 +0300

    welcome page re-navigate

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -2,17 +2,24 @@ import "./Welcome.scss";
 
 import Button, { ButtonIconLocation, ButtonTypes } from "../../components/Button";
 
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 import ArrowLong from "../../icons/arrow_long.svg?react";
 import Description from "../../components/Description";
 import { useNavigate } from "react-router-dom";
 import Flex, { GapSizes } from "../../components/Flex";
+import { useAppSelector } from "../../hooks/useAppSelector";
 
 const Welcome: FC = () => {
+	const { isAuthorized } = useAppSelector((state) => state.user);
+
 	const navigate = useNavigate();
 
 	const handleStart = () => navigate("/signin");
+
+	useEffect(() => {
+		if (isAuthorized) navigate("/app/schedule");
+	}, [isAuthorized]);
 
 	return (
 		<Flex className="welcome" column center gap={GapSizes.gap24}>
